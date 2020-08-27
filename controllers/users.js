6e742095e4795bb434d562ffb7bf630b536c2231
chenligang <1064926209@qq.com>
2020-08-27 18:19:29 +0800

    保存代码-引入处理返回函数(注意:有问题)

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,23 @@
 const query = require('./index');
+const R = require('./../lib/responseBeautifier');
+
 // 新增用户
 exports.insertData = async ctx => {
-    console.log('参数', ctx.request.body, ctx.body);
-    let _sql = "insert into users set username=?,password=?,male=?,avatar=?,email=?;"
-    // return query(_sql, value)
-    ctx.body = '123'
+    console.log('参数', ctx.request.body);
+    // let value = ctx.request.body;
+    let value = {
+        username: 'test4',
+        password: '123456',
+        email: '1064926205@qq.com'
+    }
+    let _sql = "insert into users set ?;"
+    console.log(_sql)
+    let res = await query(_sql, value);
+    if (res) {
+        console.log(R);
+        ctx.response.body = R.set(res);
+        // ctx.response.body = { status: 200, msg: '添加用户成功', data: res }
+    }
 }
 // 删除用户
 exports.deleteUserData = (username) => {
