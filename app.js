da0d7031a038d93730f00975e2163ace7ab09580
chenligang <1064926209@qq.com>
2020-10-29 18:03:41 +0800

    feat:修改了文件名

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const views = require("koa-views");
 const json = require("koa-json");
 const logger = require("koa-logger");
 const koaBody = require("koa-body");
-const Koa_Session = require("koa-session"); // 导入koa-session
+const koaSession = require("koa-session"); // 导入koa-session
 const onerror = require("koa-onerror");
 
 
 const index = require("./routes/index");
 const users = require("./routes/users");
 const R = require("./lib/responseBeautifier");
-const logUtil = require("./lib/log_util");
+const logUtil = require("./lib/logUtil");
 
 // session配置
 const session_signed_key = ["some secret hurr"]; // 这个是配合signed属性的签名key
 const session_config = {
 	key: "koa:sess" /**  cookie的key。 (默认是 koa:sess) */,
-	maxAge: 4000 /**  session 过期时间，以毫秒ms为单位计算 。*/,
+	maxAge: 3600000 /**  session 过期时间，以毫秒ms为单位计算 。*/,
 	autoCommit: true /** 自动提交到响应头。(默认是 true) */,
 	overwrite: true /** 是否允许重写 。(默认是 true) */,
 	httpOnly: true /** 是否设置HttpOnly，如果在Cookie中设置了"HttpOnly"属性，那么通过程序(JS脚本、Applet等)将无法读取到Cookie信息，这样能有效的防止XSS攻击。  (默认 true) */,
@@ -25,7 +25,7 @@ const session_config = {
 	rolling: true /** 是否每次响应时刷新Session的有效期。(默认是 false) */,
 	renew: false /** 是否在Session快过期时刷新Session的有效期。(默认是 false) */,
 };
-const session = Koa_Session(session_config, app);
+const session = koaSession(session_config, app);
 
 // onerror(app);
 app.keys = session_signed_key;
