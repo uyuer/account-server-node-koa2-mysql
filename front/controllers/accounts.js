e0190320f19da49c01235462f38afac5a0fb1aef
uyuer <13777072927@163.com>
2021-04-19 00:12:13 +0800

    feat:保存一些修改

--- front/controllers/accounts.js
+++ front/controllers/accounts.js
@@ -19,8 +19,19 @@ const addOne = async (ctx) => {
 	console.log(`请求->账户->添加一条数据: accounts.addOne; method: ${ctx.request.method}; url: ${ctx.request.url} `)
 	try {
 		let body = ctx.request.body || {};
-		// let fields = ['userId', 'website', 'websiteUrl', 'account', 'accountName', 'password', 'status', 'remark'];
-		let fields = { userId: '', website: '', websiteUrl: '', account: '', accountName: '', password: '', remark: '' };
+		let fields = {
+			userId: '', // 所属用户id
+			site: '', // 网站名称
+			website: '', // 网站地址
+			introduction: '', // 网站简介, 可以添加一些说明文字
+			account: '', // 注册账户(在网站注册的时候使用的账户)
+			password: '', // 加密密码(使用AES加密, 需要密钥来解密)
+			associates: '', // 绑定或关联的账户(注册账户关联或绑定的账户)可能有多个关联(字符串数组形式)
+			nickname: '', // 网站上的昵称
+			status: '', // 状态(0:正常,1:停用,2:注销)
+			remark: '', // 备注
+			tags: '', // 标签(对网站功能用途进行分类时使用,例如:娱乐,工作等)
+		};
 		// 校验参数并返回有效参数
 		let validParams = verifyParams(fields, body)
 		// 执行操作---
@@ -85,7 +96,7 @@ const findMultiple = async (ctx) => {
 		let ins = await schema;
 		let table = ins.getTable('accounts');
 		let userinfo = await table
-			.select('id', 'userId', 'website', 'websiteUrl', 'account', 'accountName', 'password', 'status', 'remark', 'createTime', 'updateTime')
+			.select('id', 'userId', 'site', 'website', 'introduction', 'account', 'password', 'associates', 'nickname', 'status', 'remark', 'tags', 'createTime', 'updateTime')
 			.where(`userId=:userId`)
 			.bind('userId', userId)
 			.limit(pageSize)
