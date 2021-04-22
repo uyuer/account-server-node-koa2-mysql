00cb303806b2552f7065eb9323f4e5e5093a6ad3
chenligang <1064926209@qq.com>
2021-04-22 17:59:56 +0800

    feat:更新postman

--- front/controllers/accounts.js
+++ front/controllers/accounts.js
@@ -14,6 +14,19 @@ const { formatFetch, formatFetchAll } = require("../../lib/utils");
 const config = require('./../../config');
 const { baseUploadsPath, avatarPath, avatarFullPath } = require('../../config/upload');
 
+let tableFields = {
+	userId: '', // 所属用户id
+	site: '', // 网站名称
+	website: '', // 网站地址
+	introduction: '', // 网站简介, 可以添加一些说明文字
+	account: '', // 注册账户(在网站注册的时候使用的账户)
+	password: '', // 加密密码(使用AES加密, 需要密钥来解密)
+	associates: '', // 绑定或关联的账户(注册账户关联或绑定的账户)可能有多个关联(字符串数组形式)
+	nickname: '', // 网站上的昵称
+	status: '', // 状态(0:正常,1:停用,2:注销)
+	remark: '', // 备注
+	tags: '', // 标签(对网站功能用途进行分类时使用,例如:娱乐,工作等)
+};
 // 添加一条数据
 const addOne = async (ctx) => {
 	console.log(`请求->账户->添加一条数据: accounts.addOne; method: ${ctx.request.method}; url: ${ctx.request.url} `)
@@ -52,7 +65,7 @@ const addMultiple = async (ctx) => {
 	console.log(`请求->账户->添加多条数据: accounts.addMultiple; method: ${ctx.request.method}; url: ${ctx.request.url} `)
 	try {
 		let body = ctx.request.body || {};
-		let fields = { userId: '', website: '', websiteUrl: '', account: '', accountName: '', password: '', remark: '' };
+		let fields = tableFields;
 		// 校验参数并返回有效参数
 		let validParams = body.map(item => verifyParams(fields, item))
 		// 执行操作---
