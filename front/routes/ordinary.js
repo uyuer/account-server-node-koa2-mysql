144601b9e24d3c31c00759d3e9abe84e77f1d4d7
uyuer <13777072927@163.com>
2021-05-13 21:53:52 +0800

    feat:修改登出接口请求房市

--- front/routes/ordinary.js
+++ front/routes/ordinary.js
@@ -3,7 +3,7 @@ const controller = require("./../controllers");
 
 router.post("/register", controller.ordinary.register);
 router.post("/login", controller.ordinary.login);
-router.get("/logout", controller.ordinary.logout);
+router.post("/logout", controller.ordinary.logout);
 router.post("/sendcode", controller.ordinary.sendcode);
 
 module.exports = router;
