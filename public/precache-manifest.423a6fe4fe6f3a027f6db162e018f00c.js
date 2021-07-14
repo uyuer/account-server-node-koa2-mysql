bd1889e02101196006a9a071a5d9011d4c31e956
chenligang <1064926209@qq.com>
2021-07-14 11:53:25 +0800

    feat:修改连接数据库方式,新增静态文件

---
+++ public/precache-manifest.423a6fe4fe6f3a027f6db162e018f00c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4983d94246738618275b6c4dfd6d399",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0e4dec0d3af8fad13f9",
+    "url": "/static/css/2.0433e134.chunk.css"
+  },
+  {
+    "revision": "0b724e4be5f13f00fd38",
+    "url": "/static/css/main.af7efd16.chunk.css"
+  },
+  {
+    "revision": "d0e4dec0d3af8fad13f9",
+    "url": "/static/js/2.d925ea02.chunk.js"
+  },
+  {
+    "revision": "b01d897087b6e840e163b0572a6e4eb8",
+    "url": "/static/js/2.d925ea02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b724e4be5f13f00fd38",
+    "url": "/static/js/main.d10324ae.chunk.js"
+  },
+  {
+    "revision": "77503e3445c6b72f85e0",
+    "url": "/static/js/runtime-main.5d5f2515.js"
+  },
+  {
+    "revision": "45836833440e86732281a72c7a096ac8",
+    "url": "/static/media/avatar.45836833.jpg"
+  }
+]);
