03c7d05dd9c69d2f72bf80b1aeeb0246440d021e
uyuer <13777072927@163.com>
2021-07-17 14:26:02 +0800

    feat:调试

--- middle/userverifyUnless.js
+++ middle/userverifyUnless.js
@@ -18,7 +18,7 @@ const userVerifyUnless = (params) => {
         // 如果不是被排除路径, 则需要验证用户状态, 当用户状态为冻结|
         if (result) {
             let { userId } = ctx.session.user || {};
-            console.log('需要验证用户状态', userId, ctx.request.path);
+            console.log('需要验证用户状态', userId, ctx.request.path, USERS_TABLE);
             let temp = await instanceTable(USERS_TABLE);
             let { usersTable } = temp
             console.log('1111111', temp, '1111111')
